e47052e7629ea7c4e8f4d3253a0502f5928de3ec
Jeffry Rakow <rakowj2891@forsythtech.edu>
2018-03-22 19:12:02 -0400

    data source added

--- HabitatReStoreWFApp/Properties/Settings.Designer.cs
+++ HabitatReStoreWFApp/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HabitatReStoreWFApp.Properties
-{
-
-
+namespace HabitatReStoreWFApp.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=tcp:50.63.178.68,49172;Initial Catalog=Habitat_Restore_V2;User ID=rak" +
+            "owj2891;Password=1Password!")]
+        public string Habitat_Restore_V2ConnectionString {
+            get {
+                return ((string)(this["Habitat_Restore_V2ConnectionString"]));
             }
         }
     }
