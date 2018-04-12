86f45d41deb2b46c4c2a1e03d33bcf21f5672d24
WillBAnderson <36642227+WillBAnderson@users.noreply.github.com>
2018-04-12 18:18:30 -0400

    Form updates

--- HabitatReStoreWFApp/frmDailyPickups.Designer.cs
+++ HabitatReStoreWFApp/frmDailyPickups.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rvDonations = new Microsoft.Reporting.WinForms.ReportViewer();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,16 +42,27 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // rvDonations
+            // 
+            this.rvDonations.Location = new System.Drawing.Point(13, 174);
+            this.rvDonations.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rvDonations.Name = "rvDonations";
+            this.rvDonations.ServerReport.BearerToken = null;
+            this.rvDonations.Size = new System.Drawing.Size(975, 548);
+            this.rvDonations.TabIndex = 4;
+            // 
             // frmDailyPickups
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(849, 819);
+            this.ClientSize = new System.Drawing.Size(1001, 819);
+            this.Controls.Add(this.rvDonations);
             this.Controls.Add(this.pictureBox1);
             this.Name = "frmDailyPickups";
             this.Text = "Daily Pickups";
             this.Controls.SetChildIndex(this.pictureBox1, 0);
+            this.Controls.SetChildIndex(this.rvDonations, 0);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -60,5 +72,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private Microsoft.Reporting.WinForms.ReportViewer rvDonations;
     }
 }
